d063a5caf6970b58ffc5c513c42b1030a165ed95
tracycollins <tc@threeceemedia.com>
2020-09-14 12:14:57 -0400

    Done.

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,17 @@
-export function manageFriends(state, action){
+export function manageFriends(state, action) {
+  const defaultState = { friends: [...state.friends] };
+  switch (action.type) {
+    case "ADD_FRIEND":
+      defaultState.friends.push(action.friend);
+      return defaultState;
+      break;
+    case "REMOVE_FRIEND":
+      defaultState.friends = defaultState.friends.filter((friend) => {
+        return friend.id !== action.id;
+      });
+      return defaultState;
+      break;
+    default:
+      return state;
+  }
 }
--- src/reducers/managePresents.js
+++ src/reducers/managePresents.js
@@ -1,2 +1,11 @@
-export function managePresents(state, action){
+export function managePresents(state, action) {
+  const defaultState = { numberOfPresents: 0 };
+
+  switch (action.type) {
+    case "INCREASE":
+      return { numberOfPresents: (defaultState.numberOfPresents += 1) };
+      break;
+    default:
+      return defaultState;
+  }
 }
